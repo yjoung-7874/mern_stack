adb0a33cb4acfed725bd1673f712f127ff1b2176
yjoung-7874 <people7874@gmail.com>
2021-02-24 10:58:41 +0900

    front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'connected-react-router'
+import store, {history} from './store'
+import MyRouter from './routes/Router'
+
+// bootstrap : CSS framework
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MyRouter />
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
